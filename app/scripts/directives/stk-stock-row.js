1f26618cf76694599fb0ead4f5ec70ae59a954eb
examsoft <prashant.kamath@gmail.com>
2016-01-25 16:30:01 -0600

    final set of changes for stockticker app

---
+++ app/scripts/directives/stk-stock-row.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkStockRow
+ * @description
+ * # stkStockRow
+ */
+angular.module('stockDogApp')
+    .directive('stkStockRow', function($timeout, QuoteService) {
+        return {
+            restrict: 'A',
+            require: '^stkStockTable',
+            scope: {
+                stock: '=',
+                isLast: '='
+            },
+
+            link: function($scope, $element, $attrs, stockTableCtrl) {
+                $element.tooltip({
+                    placement: 'left',
+                    title: $scope.stock.company.name
+                });
+
+                stockTableCtrl.addRow($scope);
+
+                QuoteService.register($scope.stock);
+
+                $scope.$on('$destroy', function() {
+                    stockTableCtrl.removeRow($scope);
+                    QuoteService.deregister($scope.stock);
+                });
+                // [7] If this is the last 'stock-row', fetch quotes immediately 
+                if ($scope.isLast) {
+                    $timeout(QuoteService.fetch);
+                }
+
+                $scope.$watch('stock.shares', function() {
+                    $scope.stock.marketValue = $scope.stock.shares * $scope.stock.lastPrice;
+                    $scope.stock.dayChange = $scope.stock.shares * parseFloat($scope.stock.change);
+                    $scope.stock.save();
+                });
+
+            }
+        };
+    });
--- app/scripts/services/quote-service.js
+++ app/scripts/services/quote-service.js
@@ -5,11 +5,10 @@ angular.module('stockDogApp')
         var BASE = 'http://query.yahooapis.com/v1/public/yql';
         // [1] Handles updating stock model with appropriate data from quote 
         var update = function(quotes) {
-            console.log(quotes);
             if (quotes.length === stocks.length) {
                 _.each(quotes, function(quote, idx) {
                     var stock = stocks[idx];
-                    stock.lastPrice = parseFloat(quote.LastTradePriceOnly);
+                    stock.lastPrice = parseFloat(quote.LastTradePriceOnly) + _.random(-0.5, 0.5) ;
                     stock.change = quote.Change;
                     stock.percentChange = quote.ChangeinPercent;
                     stock.marketValue = stock.shares * stock.lastPrice;
